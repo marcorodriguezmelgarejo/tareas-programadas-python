1465a90f3596ff70283d6c834e2f7500a8a8e931
Marco Rodriguez Melgarejo <mrodrguezmelgarejo@frba.utn.edu.ar>
2022-11-08 09:51:44 -0300

    Uso de hilos

--- tareas.py
+++ tareas.py
@@ -1,5 +1,6 @@
 import sched
 import time
+import threading
 
 scheduler = sched.scheduler(timefunc=time.time, delayfunc=time.sleep)
 
@@ -8,31 +9,18 @@ def tirar_error():
 
 def insertar_contrasenia_15s():
     limite = scheduler.enter(5, 1, tirar_error)
+    # crea un hilo para correr el scheduler ahi
+    hilo_tirar_error = threading.Thread(target=scheduler.run)
+    # comienza a ejecutar el hilo
+    hilo_tirar_error.start()
     contra = int(input('Cual es la contraseña? --> '))
-    scheduler.run()
     while (contra != 1234):
         print('Contraseña incorrecta, intente de nuevo.')
         contra = int(input('Cual es la contraseña? --> '))
+    # cierra el scheduler
     scheduler.cancel(limite)
+    # cierra el hilo
+    hilo_tirar_error.join()
     print('Te salvaste.')
 
 insertar_contrasenia_15s()
-
-# FUNCION DE HILOS
-# def thread_function(name):
-#     logging.info("Thread %s: starting", name)
-#     time.sleep(2)
-#     logging.info("Thread %s: finishing", name)
-
-
-# if __name__ == "__main__":
-#     format = "%(asctime)s: %(message)s"
-#     logging.basicConfig(format=format, level=logging.INFO,
-#                         datefmt="%H:%M:%S")
-#     logging.info("Main    : before creating thread")
-#     x = threading.Thread(target=thread_function, args=(1,))
-#     logging.info("Main    : before running thread")
-#     x.start()
-#     logging.info("Main    : wait for the thread to finish")
-#     # x.join()
-#     logging.info("Main    : all done")
